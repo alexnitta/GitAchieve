bc52b64012622d9742eb731f7b473fe0c8f59574
Alex Nitta <a.j.nitta@gmail.com>
2018-03-14 07:24:15 -0700

    Set up a new <UserProfileV2 />; move this.renderFriends() to a component

---
+++ client/components/userProfileV2/components/FriendList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function FriendList(props) {
+  
+  /**
+   * Displays a list of friends, or a filler message if none exist.
+   * 
+   * Required props:
+   *   `friends`: Array of `friend` objects; each must have an `id`, `avatar_url`, and `username`
+   */
+  
+  const {
+    friends,
+  } = props;
+  
+  if (friends.length !== 0) {
+    return friends.map(friend => (
+      <div key={friend.id} className="competitor-card data-result-container">
+        <img src={friend.avatar_url} className="user-avatar-med" />
+        <h3 className="font-dark-gray">{friend.username}</h3>
+      </div>
+    ));
+  }
+  
+  return <h3>Your friends will appear here once you have set up a competition.</h3>
+}
